16896a9dae2109ebc0e2b434eb5a0e814b76ed23
green804 <ginahope@gmail.com>
2021-02-17 16:21:39 -0800

    Update AmazonTest.java

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "Apple Watch";
+		String product = "Apple Watch 2";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
